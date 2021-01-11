a64e04e50562609f38aa5be940e5ece15f75c1cb
wadeepat <leaw.wadeepat@gmail.com>
2021-01-11 20:55:40 +0700

    finished

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,7 +1,12 @@
 const CourseCard = (props) => {
   return (
     <>
-      <p>This is Course card</p>
+      {/* <p>This is Course card</p> */}
+      <div id="block">
+        <p>{props.name}<button id="delete" onClick = {() =>props.del(props.name)} >delete</button></p>
+        <p>{props.grd}</p>
+        <p>{props.crd}</p>
+      </div>
     </>
   );
 };
